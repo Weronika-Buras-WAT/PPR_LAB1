f3c5087afcbc69750479cf3e2891b8b5fafe10e7
Weronika-Buras-WAT <65176525+Weronika-Buras-WAT@users.noreply.github.com>
2021-03-31 17:38:56 +0200

    Add files via upload
    jeszcze nie dokonczone

---
+++ client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			byte[] bytes = new byte[1024];
+			try {
+				IPHostEntry ipHostInfo = Dns.GetHostEntry("127.1.0.0");
+				IPAddress ipAddress = ipHostInfo.AddressList[0];
+				IPEndPoint remoteEP = new IPEndPoint(ipAddress, 12345);
+				Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp );
+				try {
+					sender.Connect(remoteEP);  
+					Console.WriteLine($"Socket connected to {sender.RemoteEndPoint.ToString()}");
+					byte[] msg = Encoding.Unicode.GetBytes("Test message");
+					sender.Send(msg);
+					Console.WriteLine($"Sent: {Encoding.Unicode.GetString(msg)}");
+					sender.Receive(bytes);
+					Console.WriteLine($"Received: {Encoding.Unicode.GetString(bytes)}");
+					sender.Shutdown(SocketShutdown.Both);  
+					sender.Close();
+				} catch (Exception e) {
+					Console.WriteLine($"Unexpected exception : {e.ToString()}");
+				}
+			} catch (Exception e) {
+				Console.WriteLine( e.ToString());
+			}
+        }
+    }
+}
