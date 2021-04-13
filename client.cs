c14590be3be5529cf577a9074eb5c3391939e054
Weronika-Buras-WAT <65176525+Weronika-Buras-WAT@users.noreply.github.com>
2021-04-13 14:04:05 +0200

    Task completed

--- client.cs
+++ client.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -14,7 +16,7 @@ namespace socket
 				IPHostEntry ipHostInfo = Dns.GetHostEntry("127.1.0.0");
 				IPAddress ipAddress = ipHostInfo.AddressList[0];
 				IPEndPoint remoteEP = new IPEndPoint(ipAddress, 12345);
-				Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp );
+				Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Dgram, ProtocolType.Udp );
 				try {
 					sender.Connect(remoteEP);  
 					Console.WriteLine($"Socket connected to {sender.RemoteEndPoint.ToString()}");
@@ -22,6 +24,10 @@ namespace socket
 					string message = Console.ReadLine();
 					Console.WriteLine("Your message");
 					Console.WriteLine(message);
+					int nProcessID = Process.GetCurrentProcess().Id;
+					Console.WriteLine("Your PID");
+					Console.WriteLine(nProcessID);
+					message = nProcessID.ToString() + message;
 					byte[] mess = Encoding.ASCII.GetBytes(message);
 					sender.Send(mess);					
 					sender.Shutdown(SocketShutdown.Both);  
--- server.c
+++ server.c
@@ -18,9 +18,8 @@ int main( void ) {
 	/* zmienne predefiniowane *****************************************/
 	int  port   = 12345;
 	
-	/* zmienne niezainicjowane ****************************************/
-	int 		n;		
-	int 		fd, newfd;
+	/* zmienne niezainicjowane ****************************************/		
+	int 		fd;
 	char 		buffer[256];
 	socklen_t 	socklen;
 	char 		hex[200];
@@ -28,12 +27,12 @@ int main( void ) {
 	struct 	sockaddr_in serv_addr, cli_addr;
 	
 	/* tworzymy gniazdo ***********************************************/
-	fd = socket( AF_INET, SOCK_STREAM, 0 );		// tworzymy nowe gniazdo
+	fd = socket( AF_INET, SOCK_DGRAM, 0 );		// tworzymy nowe gniazdo
 	if (fd < 0) 
         return error( 1, "socket()" );
         
     /* zapelniamy strukture zerami (inicjujemy) ***********************/
-	bzero( ( char * ) &serv_addr, sizeof( serv_addr ) );	// zapelniamy strukture zerami
+	bzero( ( char * )&serv_addr, sizeof(serv_addr));	// zapelniamy strukture zerami
 	
 	/* przypisujemy parametry *****************************************/
 	serv_addr.sin_family 	  	= AF_INET;			// typ gniazda 
@@ -41,7 +40,7 @@ int main( void ) {
 	serv_addr.sin_port 			= htons(port);		// port, na ktorym nasluchujemy
 	
 	/* mapujemy gniazdo ***********************************************/
-	if( bind(fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0 ) 
+	if( bind(fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0 ) 
 		return error( 2, "bind()" );
 	
 	/* rozpoczynamy nasluchiwanie na gniezdzie ************************/
@@ -50,33 +49,54 @@ int main( void ) {
 	/* kod obslugujacy nowe polaczenia ********************************/
 	socklen = sizeof(cli_addr);
 	
-	while( ( newfd = accept( fd, (struct sockaddr *) &cli_addr, &socklen ) ) ){
-		/* sprawdzamy, czy udalo sie nawiazac polaczenie **************/
-		if( newfd < 0 ){	// jesli wystapil blad, to wyswietlamy o nim informacje
-			perror( "accept()" );
-		}else{					// a jesli bledu nie bylo...
-			/* wypisujemy informacje o polaczeniu *********************/
-			printf("Address: %s Port: %d\n",  inet_ntoa( cli_addr.sin_addr ), cli_addr.sin_port );
+	while(1) 
+	{
 			
 			bzero(buffer, 256);
 
-			n = read(newfd, buffer, 255);
-
-			if( n < 0 ) 	// sprawdzamy, czy wystapil blad ....
-				perror("read()");
+			/* wypisujemy informacje o polaczeniu *********************/
 			
+			recvfrom(fd, (char *)buffer, 255, MSG_WAITALL, (struct sockaddr *)&cli_addr, &socklen);
 
-			int len = strlen(buffer);
+			printf("Address: %s Port: %d\n",  inet_ntoa( cli_addr.sin_addr ), cli_addr.sin_port );
 
-			for (int i = 0, j = 0; i < len; ++i, j += 2)
-			{
-				sprintf(hex + j, "%02x", buffer[i] & 0xff);
+			pid_t PID = fork();
+
+			char pid_number [6] = "";
+			char msg[256];
+
+			if ( PID == 0)  
+			{					
+				 for (int i = 0; i < 4; i++) 
+				 {
+				 	pid_number[i] = buffer[i];
+				 }
+				 printf("PID number: %s\n",pid_number);
+				 for (int i = 0, j = 4; i < strlen(buffer); i++, j++)
+				 	{
+				 		msg[i]=buffer[j];
+				 	}
+				printf("Message received \n");
+				FILE *file = fopen(pid_number, "w+");
+
+				if (file != NULL) 
+				{
+					int len = strlen(msg);
+					for (int i = 0, j = 0; i < len; ++i, j += 2)
+					{
+						sprintf(hex + j, "%02x", msg[i] & 0xff);
+					}
+
+					printf("File created \n");
+					fputs(hex, file);
+					fclose(file);
+				}
+				else
+				{
+					printf("error");
+				}
 			}
-
-			printf("%s\n", hex);
-			/* konczymy polaczenie ************************************/
-			close(newfd);
-		}
+					
 	}
 	 /* zamykamy gniazdo **********************************************/
 	close(fd);
